6aebafa76d96a89ea6789f97b08425c7a102d18f
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-09-28 12:42:57 +0200

    Styling card section

--- index.android.js
+++ index.android.js
@@ -3,7 +3,7 @@
 // Import a library to help create a component
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 // Create a component

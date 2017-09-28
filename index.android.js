bfac4484f6cdb2f187bd5c99e4953d0a3b146e49
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-09-28 13:12:44 +0200

    Making album list scrollable

--- index.android.js
+++ index.android.js
@@ -15,7 +15,7 @@ import AlbumList from './src/components/AlbumList';
 
 // refactored component
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>

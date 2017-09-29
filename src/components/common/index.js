3f544a40a68a7351feb772529e07e22ea0dfcec5
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-09-29 13:10:03 +0200

    Copying reusable components

---
+++ src/components/common/index.js
@@ -0,0 +1,4 @@
+export * from './Button'; // this imports and exports the Button component, on a single line
+export * from './Card';		// using this approach, means that we cannot use the 'default' export
+export * from './CardSection';	//instead, we should export them as objects
+export * from './Header';

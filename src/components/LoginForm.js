7df5381e9c3b71f159ca483ffe1323c498366dfb
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-09-29 15:24:14 +0200

    Make input as common component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,7 +12,7 @@ class LoginForm extends Component {
 					<TextInput
 						value={this.state.text} 
 						onChangeText={text => this.setState({ text })}
-						style={{ height: 20, width: 100 }}
+						style={{ height: 50, width: 100 }}
 					/>
 				</CardSection>
 				<CardSection />

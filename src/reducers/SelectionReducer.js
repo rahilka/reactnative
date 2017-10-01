615b1db01265f3dfe12767b1bd7c0e1efea9e82a
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-01 19:13:40 +0200

    more on reducers

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,3 +1,9 @@
-export default () => {
-	return null; //we do not have row selected by default
+export default (state = null, action) => {
+	// return null; //we do not have row selected by default
+	switch (action.type) {
+		case 'select_library':
+			return action.payload;
+		default: 
+			return state;
+	}
 };
